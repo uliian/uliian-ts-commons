e66d0a0dc119bdf609d7df11ecc224f466b921af
uliian <yncxcxz@126.com>
2024-05-08 14:43:34 +0800

    增加测试

--- src/hooks/useOffsetPage/index.tsx
+++ src/hooks/useOffsetPage/index.tsx
@@ -1,52 +1,63 @@
 import { DependencyList, useEffect, useState } from 'react';
 
-type OffestPageCondition = {
+export type OffestPageCondition = {
   offset: number | string;
   pageSize: number;
 };
 
-type OffsetResult<T> = {
+export type OffsetResult<T> = {
   records: T[];
   hasMore: boolean;
   offset?: number | string;
 };
 
-type Options = {
+export type Options = {
   dependencys?: DependencyList;
   pageSize?: number;
   ready?: () => boolean;
 };
 
+export type UseOffsetPageResult<T> = {
+  offset?: number | string,
+    records: T[],
+    hasMore: boolean,
+    loading: boolean,
+    getMore: () => Promise<void>,
+}
+
 export default function useOffsetPage<T>(
   service: (page: OffestPageCondition) => Promise<OffsetResult<T>>,
   params:Record<string,any>,
   options: Options,
-) {
+):UseOffsetPageResult<T> {
   const [offset, setOffset] = useState<number | string>();
   const [records, setRecords] = useState<T[]>([]);
   const [hasMore, setHasMore] = useState<boolean>(false);
   const { dependencys, pageSize, ready } = options;
   const [loading, setLoading] = useState(false);
 
-  const run = () => {
+  const run:()=>Promise<void> = () =>  {
     if (ready?.() ?? true) {
-      service({ ...params, offset: offset ?? '0', pageSize: pageSize ?? 20 }).then((result) => {
+      setLoading(true)
+      return service({ ...params, offset: offset ?? '0', pageSize: pageSize ?? 20 }).then((result) => {
         setOffset(result.offset);
         setRecords(preValue =>[...preValue, ...result.records]);
         setHasMore(result.hasMore);
         setLoading(false);
       });
     }
+    setLoading(false)
+    return Promise.resolve()
   };
 
   const getMore = run;
 
   useEffect(() => {
-    setOffset(undefined);
+    setOffset("0");
     setRecords([]);
     setHasMore(false);
     run()
-  }, [params,...dependencys??[]]);
+  }, [...(dependencys??[])]);
 
   return {
     offset,
--- test/useOffsetPage.test.ts
+++ test/useOffsetPage.test.ts
@@ -1,7 +1,52 @@
-import { renderHook } from "@testing-library/react";
-import useOffsetPage from "../src/hooks/useOffsetPage";
+import { RenderHookResult, renderHook, waitFor } from "@testing-library/react";
+import useOffsetPage, { OffestPageCondition, OffsetResult, Options, UseOffsetPageResult } from "../src/hooks/useOffsetPage";
+import { act } from "react-dom/test-utils";
 
-// test("useOffsetPage_first", () => {
-//     const { result } = renderHook(() => useOffsetPage<any>((page)=>));
-    
-// })
+
+describe("useOffsetPage", () => {
+    const setUp = (service: (page: OffestPageCondition) => Promise<OffsetResult<string>>, params: Record<string, any>, options: Options) => renderHook(() => useOffsetPage<string>(service, params, options));
+
+    let hook: RenderHookResult<UseOffsetPageResult<string>, {}>
+    it("useOffsetPage auto begin", async () => {
+        act(() => {
+            hook = setUp((page) => testService(page), {}, {
+            })
+
+        })
+        await waitFor(() => {
+            expect(hook.result.current.records.length).toBe(3);
+            expect(hook.result.current.hasMore).toBe(true);
+            expect(hook.result.current.offset).toBe(3);
+        })
+
+        await act(async () => {
+            await hook.result.current.getMore()
+        })
+        // expect(hook.result.current.loading).toBe(false);
+        // expect(hook.result.current.records.length).toBe(1)
+
+        await waitFor(() => {
+            expect(hook.result.current.records.length).toBe(4);
+            expect(hook.result.current.hasMore).toBe(false);
+        })
+    })
+})
+
+
+
+const testService = (page: OffestPageCondition) => {
+    console.log("page", page)
+    if (page.offset.toString() === "0") {
+        return Promise.resolve({
+            records: ["1", "2", "3"],
+            hasMore: true,
+            offset: 3
+        })
+    } else {
+        return Promise.resolve({
+            records: ["4"],
+            hasMore: false,
+            offset: 4
+        })
+    }
+}
